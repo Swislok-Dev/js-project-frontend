024a26c2fe3cf73948da5985cf93ba8fba20b007
Cory Marker <cmarker4@gmail.com>
2021-09-13 00:20:38 -0500

    render User form and handle submit

--- src/index.js
+++ src/index.js
@@ -1,4 +1,38 @@
 const api = new ApiService("http://localhost:3000")
 const modal = new Modal()
+let user
+const main = document.getElementById("main")
 
-Guitar.getGuitars()
+createUsernameForm()
+
+
+document.querySelector("form").addEventListener("submit", handleUsernameSubmit)
+
+function createUsernameForm() {
+  // main.innerHTML = ""
+  main.innerHTML =`
+    <form>
+      <label for="username">Username</label>
+      <input type="text" id="username" name="username"><br>
+      <input type="submit" value="Submit">
+    </form>
+    `
+}
+
+
+
+
+function handleUsernameSubmit(e) {
+  e.preventDefault() 
+  const regex = /^[A-Za-z]+$/;
+  if (e.target.username.value.match(regex)) {
+    api.findOrCreateUser(e.target.username.value).then(userData => {
+      user = userData
+      Guitar.getGuitars()
+    })
+  } else {
+      alert("Please enter a username")
+      e.target.reset()
+      return(createUsernameForm)
+  } 
+}
