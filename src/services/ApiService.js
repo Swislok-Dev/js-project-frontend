61861d6737f9b92e45d8df8655e5bcc8d8df183b
Cory Marker <cmarker4@gmail.com>
2021-09-13 00:22:29 -0500

    add findOrCreateUser function to POST to db

--- src/services/ApiService.js
+++ src/services/ApiService.js
@@ -3,23 +3,40 @@ class ApiService {
     this.api = api
   }
 
-  getUsers = () => {
-    return fetch(this.api + "/users")
-    .then(response => response.json())
-  }
-
-
+  
+  
   getGuitars = () => {
     return fetch(this.api + "/guitars")
     .then(response => response.json())
   }
+
+  getUsers = () => {
+    return fetch(this.api + "/users")
+    .then(response => response.json())
+  }
+  
   newPost = (post) => {
+    post.user_id = user.id
     return fetch(this.api + "/guitars", {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json'
       },
       body: JSON.stringify(post)
+    })
+    .then(response => response.json())
+  }
+  
+  findOrCreateUser = (username) => {
+    // if (username === undefined || username === null) {
+    //   return
+    // }
+    return fetch(this.api + "/users", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({username: username}),
     })
     .then(response => response.json())
   }
