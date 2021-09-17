5e83b15743133b461143b4a30299d0842867a09a
Cory Marker <cmarker4@gmail.com>
2021-09-16 21:09:11 -0500

    Clean up spacing

--- src/index.js
+++ src/index.js
@@ -8,8 +8,8 @@ createUsernameForm()
 
 
 document.querySelector("form").addEventListener("submit", handleUsernameSubmit)
+
 function createUsernameForm() {
-  // main.innerHTML = ""
   main.innerHTML =`
   <form>
   <label for="username">Username</label><br>
@@ -19,31 +19,25 @@ function createUsernameForm() {
   `
 }
 
-
-
-
 function handleUsernameSubmit(e) {
-    e.preventDefault()
-    const regex = /^[A-Za-z0-9-_]+$/;
-    if (e.target.username.value.match(regex)) {
-      api.findOrCreateUser(e.target.username.value).then(userData => {
-        user = userData
-        document.getElementById("account-button").style.display = "grid"
-        document.getElementById("header-2").innerText = "Showcase your guitars"
-        Guitar.getGuitars()
-        User.getUsers()
-        document.getElementById("account-button").addEventListener("click", showUser)
-      })
-    } else {
-      alert("Please enter a username")
-      e.target.reset()
-      return(createUsernameForm)
-    }
-    
-    
-  }
-  
+  e.preventDefault()
+  const regex = /^[A-Za-z0-9-_]+$/;
+  if (e.target.username.value.match(regex)) {
+    api.findOrCreateUser(e.target.username.value).then(userData => {
+      user = userData
+      document.getElementById("account-button").style.display = "grid"
+      document.getElementById("header-2").innerText = "Showcase your guitars"
+      Guitar.getGuitars()
+      User.getUsers()
+      document.getElementById("account-button").addEventListener("click", showUser)
+    })
+  } else {
+    alert("Please enter a username")
+    e.target.reset()
+    return(createUsernameForm)
+  }  
+}
+
 const showUser = () => {
   api.showUser(user).then(User.showUser(user))
-  
-};
+}
