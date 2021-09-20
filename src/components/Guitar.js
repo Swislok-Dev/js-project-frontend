9112484c43f330f076ec0ddfa57251f752630583
Cory Marker <cmarker4@gmail.com>
2021-09-20 00:12:10 -0500

    show guitar page & made radio for new post

--- src/components/Guitar.js
+++ src/components/Guitar.js
@@ -7,16 +7,34 @@ class Guitar {
     this.constructor.all.push(this)
   }
 
-
   renderCard = () => {
-    const {style, brand, model, imageUrl, id, username} = this.data
+    const {brand, model, imageUrl, id} = this.data
     document.getElementById("guitar-container").innerHTML += `
     <div class="guitar-card" data-id=${id}>
       <img src=${imageUrl} alt=${brand} ${model}/>
       <p class='guitar-name'>${brand} </p>
       <p>${model}</p>
-      <p>${style}</p>
+      <button>View</button>
+    </div>
+  `
+  }
+
+  showGuitar = () => {
+    const {style, brand, model, imageUrl, id, username, createdAt} = this.data
+    const header2 = document.getElementById("header-2")
+    header2.innerHTML = `${brand} ${model} <br>
+    <button id="back-button">Go Back</button>`
+    document.getElementById("back-button").addEventListener("click", Guitar.getGuitars)
+    document.querySelector("button.add-guitar").style.display = "none"
+
+
+    document.getElementById("guitar-container").innerHTML = `
+    <div class="guitar-card" data-id=${id}>
+      <img src=${imageUrl} alt=${brand} ${model}/>
+      <p>Type: ${style}</p>
+      <p>Posted: ${createdAt}</p>
       <p>Posted by: ${username}</p>
+      <button>Like</button>
     </div>
   `
   }
@@ -31,7 +49,6 @@ class Guitar {
     })
   }
 
-  
   static handlePost = (e) => {
     e.preventDefault()
     const createGuitar = {
@@ -45,22 +62,27 @@ class Guitar {
     e.target.reset()
   }
   
-  
   static openNewProductForm = () => {
 
     modal.main.innerHTML = `
     <h2>Post your product</h2>
     <form>
-    <label for="username">Username</label><br>
-    <input type="text" name="username" value=${user.username}><br>
     <label for="brand">Brand:</label><br>
-    <input type="text" name="brand"><br>
+    <input type="text" name="brand" required><br>
     <label for="model">Model:</label><br>
-    <input type="text" name="model"><br>
-    <label for="style">Style:</label><br>
-    <input type="text" name="style"><br>
+    <input type="text" name="model" required><br>
     <label for="imageUrl">Image URL:</label><br>
-    <input type="text" name="imageUrl"></br>
+    <input type="text" name="imageUrl" required></br>
+    
+    <label for="style">Style:</label><br>
+    <input type="radio" name="style" value="Electric" required>
+    <label for="Electric">Electric</label>
+    <input type="radio" name="style" value="Acoustic" required>
+    <label for="Acoustic">Acoustic</label>
+    <input type="radio" name="style" value="Acoustic-electric" required>
+    <label for="Acoustic-electric">Acoustic-electric</label><br>
+
+    
     <input type="submit" value="Post"><br>
     </form>
     `
@@ -73,20 +95,27 @@ class Guitar {
     document.getElementById("header-2").innerHTML = "Showcase your guitars"
     const main = document.getElementById('main')
     main.innerHTML = ""
+
     const guitarContainer = document.createElement('div')
     guitarContainer.id = "guitar-container"
+
     const addPost = document.createElement('button')
     addPost.innerText = "Add a guitar"
     addPost.classList.add("add-guitar")
     addPost.addEventListener("click", this.openNewProductForm)
+
     main.append(addPost, guitarContainer)
     this.all.forEach(guitar => guitar.renderCard())
+    guitarContainer.addEventListener("click", this.handleShowGuitar)
   }
-  
-  
-  
-  
-  
-  
+
+  static handleShowGuitar = (e) => {
+    if (e.target.tagName == "IMG" || e.target.innerText == "View") {
+      let id = e.target.closest(".guitar-card").dataset.id
+      id = parseInt(id)
+      console.log(`The ID is currently set to ${id}`)
+      Guitar.find(id).showGuitar()
+    }
+  }
   
 }
